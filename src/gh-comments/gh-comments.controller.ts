6740736f83f46e41d79ed045c214d2401c00cc74
Emanuel Avila <avilaemanueel@gmail.com>
2024-11-16 23:06:28 -0300

    update controller logic

--- src/gh-comments/gh-comments.controller.ts
+++ src/gh-comments/gh-comments.controller.ts
@@ -6,8 +6,11 @@ import {
   Patch,
   Param,
   Delete,
+  Res,
+  HttpStatus,
 } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 
 import { GhCommentsService } from './gh-comments.service';
 import { CreateGhCommentDto } from './dto/create-gh-comment.dto';
@@ -24,8 +27,17 @@ export class GhCommentsController {
     description: 'The gh-comment has been successfully created.',
   })
   @Post()
-  create(@Body() createGhCommentDto: CreateGhCommentDto) {
-    return this.ghCommentsService.create(createGhCommentDto);
+  async create(
+    @Res() response: Response,
+    @Body() createGhCommentDto: CreateGhCommentDto,
+  ) {
+    const createdGhComment =
+      await this.ghCommentsService.create(createGhCommentDto);
+
+    return response.status(HttpStatus.CREATED).json({
+      message: 'Gh-comment created successfully',
+      id: createdGhComment._id,
+    });
   }
 
   @ApiOperation({ summary: 'Get all gh-comments' })
@@ -34,8 +46,13 @@ export class GhCommentsController {
     description: 'The gh-comments have been successfully retrieved.',
   })
   @Get()
-  findAll() {
-    return this.ghCommentsService.findAll();
+  async findAll(@Res() response: Response) {
+    const ghComments = await this.ghCommentsService.findAll();
+
+    return response.status(HttpStatus.OK).json({
+      message: 'Gh-comments retrieved successfully',
+      comments: ghComments,
+    });
   }
 
   @ApiOperation({ summary: 'Get a gh-comment by id' })
@@ -44,8 +61,13 @@ export class GhCommentsController {
     description: 'The gh-comment has been successfully retrieved.',
   })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ghCommentsService.findOne(id);
+  async findOne(@Res() response: Response, @Param('id') id: string) {
+    const ghComment = await this.ghCommentsService.findOne(id);
+
+    return response.status(HttpStatus.OK).json({
+      message: 'Gh-comment retrieved successfully',
+      comments: ghComment,
+    });
   }
 
   @ApiOperation({ summary: 'Update a gh-comment by id' })
@@ -54,11 +76,20 @@ export class GhCommentsController {
     description: 'The gh-comment has been successfully updated.',
   })
   @Patch(':id')
-  update(
+  async update(
+    @Res() response: Response,
     @Param('id') id: string,
     @Body() updateGhCommentDto: UpdateGhCommentDto,
   ) {
-    return this.ghCommentsService.update(id, updateGhCommentDto);
+    const updatedGhComment = await this.ghCommentsService.update(
+      id,
+      updateGhCommentDto,
+    );
+
+    return response.status(HttpStatus.OK).json({
+      message: 'Gh-comment updated successfully',
+      id: updatedGhComment?._id,
+    });
   }
 
   @ApiOperation({ summary: 'Delete a gh-comment by id' })
@@ -67,7 +98,12 @@ export class GhCommentsController {
     description: 'The gh-comment has been successfully deleted.',
   })
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ghCommentsService.remove(id);
+  async remove(@Res() response: Response, @Param('id') id: string) {
+    const deletedGhComment = await this.ghCommentsService.remove(id);
+
+    return response.status(HttpStatus.OK).json({
+      message: 'Gh-comment deleted successfully',
+      id: deletedGhComment?._id,
+    });
   }
 }
