a6cb34aaa0c2e3c242fbcf36465da44b70f1afed
Emanuel Avila <avilaemanueel@gmail.com>
2024-12-25 16:16:34 -0300

    wip: repositories stuffs

--- src/gh-repositories/gh-repositories.controller.ts
+++ src/gh-repositories/gh-repositories.controller.ts
@@ -1,4 +1,6 @@
+import { CurrentUser } from '@/auth/decorators/current-user.decorator';
 import { JwtAuthGuard } from '@/auth/jwt/jwt-auth.guard';
+import { User } from '@/user/entities/user.entity';
 import { Controller, Get, Res, UseGuards } from '@nestjs/common';
 import {
   ApiBearerAuth,
@@ -22,8 +24,51 @@ export class GhRepositoriesController {
     description: 'The repositories have been successfully retrieved.',
   })
   @Get()
-  async getRepositories(@Res() response: Response) {
-    const repositories = await this.ghRepositoriesService.getRepositories();
+  async getRepositories(@Res() response: Response, @CurrentUser() user: User) {
+    const repositories = await this.ghRepositoriesService.getRepositories(
+      String(user.github_id),
+    );
+
+    return response.status(200).json({
+      repositories,
+    });
+  }
+
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get all repositories from remote github' })
+  @ApiResponse({
+    status: 200,
+    description: 'The repositories have been successfully retrieved.',
+  })
+  @Get('remote')
+  async getRemoteRepositories(
+    @Res() response: Response,
+    @CurrentUser() user: User,
+  ) {
+    const repositories =
+      await this.ghRepositoriesService.findRemoteRepositories(
+        String(user.github_id),
+      );
+
+    return response.status(200).json({
+      repositories,
+    });
+  }
+
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get all repositories from local mongodb' })
+  @ApiResponse({
+    status: 200,
+    description: 'The repositories have been successfully retrieved.',
+  })
+  @Get('local')
+  async getLocalRepositories(
+    @Res() response: Response,
+    @CurrentUser() user: User,
+  ) {
+    const repositories = await this.ghRepositoriesService.findLocalRepositories(
+      String(user.github_id),
+    );
 
     return response.status(200).json({
       repositories,
