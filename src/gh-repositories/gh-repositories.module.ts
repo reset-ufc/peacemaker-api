9e828ed62f48a1252aef184ae655fc108e5d552f
Emanuel Avila <avilaemanueel@gmail.com>
2024-12-25 16:16:11 -0300

    update imports

--- src/gh-repositories/gh-repositories.module.ts
+++ src/gh-repositories/gh-repositories.module.ts
@@ -1,8 +1,25 @@
+import { UserModule } from '@/user/user.module';
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { GhRepositoriesService } from './gh-repositories.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  GhRepository,
+  GhRepositorySchema,
+} from './entities/gh-repository.entity';
 import { GhRepositoriesController } from './gh-repositories.controller';
+import { GhRepositoriesService } from './gh-repositories.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: GhRepository.name,
+        schema: GhRepositorySchema,
+      },
+    ]),
+    UserModule,
+    HttpModule,
+  ],
   controllers: [GhRepositoriesController],
   providers: [GhRepositoriesService],
 })
