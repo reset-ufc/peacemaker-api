052e2e82c21da292668ac9248bafe5d84328b2b4
Emanuel Avila <avilaemanueel@gmail.com>
2025-01-03 11:48:45 -0300

    remove useless decorator

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,10 +1,9 @@
 import { Controller, Get, Patch, Post, Req } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
-@ApiBearerAuth()
 @ApiTags('User')
 @Controller('v1/user')
 export class UserController {
