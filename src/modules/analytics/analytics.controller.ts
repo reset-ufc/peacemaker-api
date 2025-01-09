fabcf57cead8f4d304b1ebb896674ce2382bf0e8
Emanuel Avila <avilaemanueel@gmail.com>
2025-01-09 20:18:02 -0300

    update analytics controller

--- src/modules/analytics/analytics.controller.ts
+++ src/modules/analytics/analytics.controller.ts
@@ -1,40 +1,21 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { AnalyticsService } from './analytics.service';
 
 @ApiTags('Analytics')
 @Controller('v1/analytics')
 export class AnalyticsController {
-  @Get()
-  // TODO: implement
-  getAllAnalytics() {
-    return {
-      totalRepositories: 120,
-      averageCommentsToxicity: 0.45,
-      medianCommentsToxicity: 0.2,
-      totalComments: 1300,
-      resolvedComments: 100,
+  constructor(private readonly analyticsService: AnalyticsService) {}
 
-      moderationActivity: [
-        {
-          sentiment: 'toxic',
-          comments: 10,
-        },
-        {
-          sentiment: 'neutral',
-          comments: 20,
-        },
-        {
-          sentiment: 'positive',
-          comments: 30,
-        },
-      ],
-    };
+  // TODO: implement
+  @Get()
+  getAllAnalytics() {
+    return this.analyticsService.getAllAnalytics();
   }
 
+  // TODO: implement
   @Get(':repository_id')
   getAnalytics(@Param('repository_id') repositoryId: string) {
-    return {
-      id: 'analysis1',
-    };
+    return this.analyticsService.getAnalytics(repositoryId);
   }
 }
