7c1c7631b6961bebcce31fbe52bfc73c3c87471b
Emanuel Avila <avilaemanueel@gmail.com>
2025-01-09 20:17:37 -0300

    update controller

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,45 +1,37 @@
-// comments.controller.ts
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { CommentService } from './comment.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @ApiTags('Comments')
 @Controller('v1/comments')
 export class CommentController {
+  constructor(private readonly commentService: CommentService) {}
+
   @Get(':repository_id')
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   getComments(@Param('repository_id') repositoryId: string) {
-    return [{ id: 'comment1', content: 'Example comment', user: 'user123' }];
+    return this.commentService.findAll(repositoryId);
   }
 
   @Get(':id')
   getComment(@Param('id') id: string) {
-    return { id, content: 'Example comment', user: 'user123' };
+    return this.commentService.findOne(id);
   }
 
   @Post(':repository_id')
   createComment(
     @Param('repository_id') repositoryId: string,
-    @Body() body: { content: string; user: string },
+    @Body() createCommentDto: CreateCommentDto,
   ) {
-    return { id: 'new_comment', content: body.content, user: body.user };
+    return this.commentService.create(repositoryId, createCommentDto);
   }
 
   @Patch(':repository_id/:id')
-  updateComment(@Param('id') id: string, @Body() body: { content: string }) {
-    return { id, content: body.content };
-  }
-
-  @Delete(':repository_id/:id')
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  deleteComment(@Param('id') id: string) {
-    return { message: 'Comment deleted successfully' };
+  updateComment(
+    @Param('id') id: string,
+    @Body() updateCommentDto: UpdateCommentDto,
+  ) {
+    return this.commentService.update(id, updateCommentDto);
   }
 }
