55945782b712dc8ec3b11b8feac1095888039beb
Emanuel Avila <avilaemanueel@gmail.com>
2025-01-09 20:18:18 -0300

    import schema

--- src/modules/analytics/analytic.module.ts
+++ src/modules/analytics/analytic.module.ts
@@ -1,9 +1,21 @@
+import {
+  Comment,
+  CommentSchema,
+} from '@/modules/comment/entities/comment.entity';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Comment.name,
+        schema: CommentSchema,
+      },
+    ]),
+  ],
   controllers: [AnalyticsController],
   providers: [AnalyticsService],
   exports: [AnalyticsService],
