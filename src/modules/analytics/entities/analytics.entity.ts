04e9be288b8a05b909102df9cd0254c3385cb064
Emanuel Avila <avilaemanueel@gmail.com>
2025-01-23 01:29:47 -0300

    update entity

--- src/modules/analytics/entities/analytics.entity.ts
+++ src/modules/analytics/entities/analytics.entity.ts
@@ -1,22 +1,28 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import mongoose from 'mongoose';
 
-export type AnalyticsDocument = Analytics & Document;
+export type AnalyticsDocument = mongoose.Document;
 
 @Schema()
 export class Analytics {
+  @ApiProperty()
   @Prop()
   user_id: string;
 
+  @ApiProperty()
   @Prop()
   average_score: number;
 
+  @ApiProperty()
   @Prop()
   total_comments: number;
 
+  @ApiProperty()
   @Prop()
   removed_comments: number;
 
+  @ApiProperty()
   @Prop()
   absolute_comments: number;
 }
