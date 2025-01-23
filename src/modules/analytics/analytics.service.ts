e95269e573cadbfeda9ec45ebea995126cdf9b1c
Emanuel Avila <avilaemanueel@gmail.com>
2025-01-23 01:24:52 -0300

    fixed model usage

--- src/modules/analytics/analytics.service.ts
+++ src/modules/analytics/analytics.service.ts
@@ -1,7 +1,4 @@
-import {
-  Comment,
-  CommentDocument,
-} from '@/modules/comment/entities/comment.entity';
+import { Comment } from '@/modules/comment/entities/comment.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -10,7 +7,7 @@ import { Model } from 'mongoose';
 export class AnalyticsService {
   constructor(
     @InjectModel(Comment.name)
-    private readonly commentModel: Model<CommentDocument>,
+    private readonly commentModel: Model<Comment>,
   ) {}
 
   private async getComments(userId: number, repositoryId: string) {
