19b5fbb6b9d7f0eceae874b2926a7eb6573e6324
Emanuel Avila <avilaemanueel@gmail.com>
2025-01-23 01:10:14 -0300

    fixe typo

--- src/modules/analytics/analytics.module.ts
+++ src/modules/analytics/analytics.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Comment, CommentSchema } from '../comment/entities/comment.entity';
+import { UserModule } from '../user/user.module';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
 
 @Module({
   imports: [
+    UserModule,
     MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
   ],
   controllers: [AnalyticsController],
