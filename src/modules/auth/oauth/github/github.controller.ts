6cbb6d3e96a5b4e45f32f1954fd741739ae48f9a
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-03 12:52:28 -0300

    refatora controlador do Github OAuth para incluir novos parâmetros de consulta e ajustar o tratamento de resposta

--- src/modules/auth/oauth/github/github.controller.ts
+++ src/modules/auth/oauth/github/github.controller.ts
@@ -1,25 +1,41 @@
 import { IsPublic } from '@/common/decorators/is-public.decorator';
-import { Controller, Get, Query, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 import { ApiQuery, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import { GithubService } from './github.service';
 
 @ApiTags('Github OAuth')
-@Controller('v1/oauth/github')
+@Controller('oauth/github')
 export class GithubController {
   constructor(private readonly githubService: GithubService) {}
 
   @IsPublic()
   @ApiQuery({
-    name: 'callback_url',
+    name: 'client_type',
     type: String,
     nullable: true,
     required: false,
   })
+  @ApiQuery({
+    name: 'redirect_uri',
+    type: String,
+    nullable: true,
+    required: true,
+  })
   @Get('')
-  autorization(@Query('callback_url') callbackUrl?: string) {
-    const autorizationUrl = this.githubService.authorization(callbackUrl);
-    return autorizationUrl;
+  autorization(
+    @Res({ passthrough: true }) response: Response,
+    @Query('client_type') clientType: 'web' | 'extension' = 'web',
+    @Query('redirect_uri') redirectUri: string = '',
+  ) {
+    const stateData = { client_type: clientType, redirect_uri: redirectUri };
+    const state = Buffer.from(JSON.stringify(stateData)).toString('base64');
+
+    const autorizationUrl = this.githubService.authorization(state);
+
+    return response.status(HttpStatus.OK).json({
+      url: autorizationUrl,
+    });
   }
 
   @IsPublic()
@@ -28,18 +44,29 @@ export class GithubController {
   async callback(
     @Res({ passthrough: true }) response: Response,
     @Query('code') code: string,
+    @Query('state') state: string,
   ) {
     const token = await this.githubService.callback(code);
+    const stateDecoded = JSON.parse(
+      Buffer.from(state, 'base64').toString(),
+    ) as {
+      client_type: string;
+      redirect_uri: string;
+    };
 
-    response.cookie('access_token', token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none',
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    });
+    if (stateDecoded.client_type !== 'web') {
+      response.status(HttpStatus.OK).json({
+        access_token: token,
+      });
+    } else {
+      response.cookie('access_token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'lax',
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      });
 
-    response.setHeader('Autorization', `Bearer ${token}`);
-
-    return { token };
+      response.redirect(stateDecoded.redirect_uri);
+    }
   }
 }
