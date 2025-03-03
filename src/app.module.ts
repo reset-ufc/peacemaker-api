32a9e98f60b90eb14b7b77bf3e22e7e82138b62f
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-03 14:21:55 -0300

    refatora módulo principal: substitui módulos e controladores antigos por novos módulos de comentários, feedbacks, métricas, repositórios e sugestões

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,44 +4,42 @@ import { CoreModule } from './core/core.module';
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
-import { AnalyticsController } from './modules/analytics/analytics.controller';
-import { AnalyticsModule } from './modules/analytics/analytics.module';
 import { JwtAuthModule } from './modules/auth/jwt/jwt-auth.module';
 import { JwtAuthStrategy } from './modules/auth/jwt/jwt-auth.strategy';
 import { GithubController } from './modules/auth/oauth/github/github.controller';
 import { GithubModule } from './modules/auth/oauth/github/github.module';
-import { CommentController } from './modules/comment/comment.controller';
-import { CommentModule } from './modules/comment/comment.module';
-import { RepositoryController } from './modules/repository/repository.controller';
-import { RepositoryModule } from './modules/repository/repository.module';
-import { UserController } from './modules/user/user.controller';
-import { UserModule } from './modules/user/user.module';
+import { CommentsController } from './modules/comments/comments.controller';
+import { CommentsModule } from './modules/comments/comments.module';
+import { FeedbacksController } from './modules/feedbacks/feedbacks.controller';
+import { FeedbacksModule } from './modules/feedbacks/feedbacks.module';
+import { MetricsModule } from './modules/metrics/metrics.module';
+import { RepositoriesController } from './modules/repositories/repositories.controller';
+import { RepositoriesModule } from './modules/repositories/repositories.module';
+import { SuggestionsController } from './modules/suggestions/suggestions.controller';
+import { SuggestionsModule } from './modules/suggestions/suggestions.module';
+import { UsersController } from './modules/users/users.controller';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigSetupModule,
-    CoreModule,
-    UserModule,
-    GithubModule,
-    CommentModule,
-    // TODO: Maybe we should delete this module
-    // ClassificationModule,
-    // TODO: Maybe we should delete this module
-    // SuggestionModule,
-    RepositoryModule,
-    AnalyticsModule,
     JwtAuthModule,
+    GithubModule,
+    CoreModule,
+    RepositoriesModule,
+    CommentsModule,
+    SuggestionsModule,
+    FeedbacksModule,
+    MetricsModule,
+    UsersModule,
   ],
   controllers: [
-    UserController,
     GithubController,
-    AnalyticsController,
-    CommentController,
-    // TODO: Maybe we should delete this module
-    // ClassificationController,
-    // TODO: Maybe we should delete this module
-    // SuggestionController,
-    RepositoryController,
+    UsersController,
+    RepositoriesController,
+    CommentsController,
+    SuggestionsController,
+    FeedbacksController,
   ],
   providers: [
     {
