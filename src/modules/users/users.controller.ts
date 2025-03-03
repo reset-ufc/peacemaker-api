7e934ed25c385130b6efa1147ebd3c7a5226508e
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-03 12:57:29 -0300

    adiciona endpoint para obter perfil do usuário com base no ID do GitHub

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(readonly userService: UsersService) {}
+
+  @Get('me')
+  async profile(@Req() request: Request) {
+    const user = request.user as User;
+    const fullUser = await this.userService.findOneByGithubId(user.github_id);
+
+    if (!fullUser) {
+      return { profile: null };
+    }
+
+    const userProfile = {
+      github_id: fullUser.github_id,
+      username: fullUser.username,
+      name: fullUser.name,
+      email: fullUser.email,
+      avatar_url: fullUser.avatar_url,
+    };
+
+    return { profile: userProfile };
+  }
+}
