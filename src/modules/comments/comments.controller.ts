3c3114d72c639c916c82f812e615084bc34be307
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-04 00:01:03 -0300

    refatora controlador de comentários: remove dependência de Response e simplifica retorno

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -1,7 +1,6 @@
 import { IsPublic } from '@/common/decorators/is-public.decorator';
-import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { Response } from 'express';
 import { CommentsService } from './comments.service';
 
 @ApiTags('Comments')
@@ -11,28 +10,28 @@ export class CommentsController {
 
   @IsPublic()
   @Get('')
-  async getComments(@Res() response: Response) {
+  async getComments() {
     const comments = await this.commentService.findAll();
 
-    return response.status(HttpStatus.OK).json(comments);
+    return { comments };
   }
 
   @IsPublic()
-  @Get(':repository_id')
+  @Get('repository/:repository_id')
   async getCommentsByRepositoryId(
-    @Res() response: Response,
     @Param('repository_id') repositoryId: string,
   ) {
     const comments =
       await this.commentService.findAllByRepository(repositoryId);
-    return response.status(HttpStatus.OK).json(comments);
+
+    return { comments };
   }
 
   @IsPublic()
   @Get(':id')
-  async getComment(@Res() response: Response, @Param('id') id: string) {
+  async getComment(@Param('id') id: string) {
     const comment = await this.commentService.findOne(id);
 
-    return response.status(HttpStatus.OK).json(comment);
+    return { comment };
   }
 }
