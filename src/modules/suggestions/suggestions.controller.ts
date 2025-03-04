106847ac6a26ba420008b47b985946bd2e463689
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-03 22:25:47 -0300

    implementa controlador de sugestões: adiciona métodos para obter sugestões e busca por repositório

--- src/modules/suggestions/suggestions.controller.ts
+++ src/modules/suggestions/suggestions.controller.ts
@@ -1,6 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { SuggestionsService } from './suggestions.service';
 
 @ApiTags('Suggestions')
 @Controller('suggestions')
-export class SuggestionsController {}
+export class SuggestionsController {
+  constructor(private readonly suggestionService: SuggestionsService) {}
+
+  @Get()
+  getAll() {
+    const suggestions = this.suggestionService.getAll();
+
+    return suggestions;
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string) {
+    const suggestion = this.suggestionService.getById(id);
+
+    return suggestion;
+  }
+
+  @Get('repository/:repositoryId')
+  getAllByRepository(@Param('repositoryId') repositoryId: string) {
+    const suggestions = this.suggestionService.getAllByRepository(repositoryId);
+
+    return suggestions;
+  }
+}
