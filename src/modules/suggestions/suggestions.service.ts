eb989af1cfa69fd487f0c9d947c4610c7a3264ed
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-03 22:25:41 -0300

    implementa serviço de sugestões: adiciona métodos para buscar sugestões e injeção de dependência do modelo

--- src/modules/suggestions/suggestions.service.ts
+++ src/modules/suggestions/suggestions.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Suggestion } from './entities/suggestion.entity';
 
 @Injectable()
-export class SuggestionsService {}
+export class SuggestionsService {
+  constructor(
+    @InjectModel(Suggestion.name)
+    private readonly suggestionModel: Model<Suggestion>,
+  ) {}
+
+  async getAll(): Promise<Suggestion[] | []> {
+    const suggestions = await this.suggestionModel.find().exec();
+
+    return suggestions;
+  }
+
+  async getAllByRepository(repositoryId: string): Promise<Suggestion[] | []> {
+    const suggestions = await this.suggestionModel
+      .find({ repository_id: repositoryId })
+      .exec();
+
+    return suggestions;
+  }
+
+  async getById(id: string): Promise<Suggestion | null> {
+    const suggestion = await this.suggestionModel.findOne({ _id: id }).exec();
+
+    return suggestion;
+  }
+}
