ffdbb61235ffe71eec35b6d19688903e955cbe2a
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-07 20:59:55 -0300

    atualiza a entidade de comentário para incluir novos campos e remove referências a repositórios e usuários

--- src/modules/comments/entities/comment.entity.ts
+++ src/modules/comments/entities/comment.entity.ts
@@ -1,55 +1,96 @@
-import { Repository } from '@/modules/repositories/entities/repository.entity';
-import { User } from '@/modules/users/entities/user.entity';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 import { Document, Types } from 'mongoose';
-import { Categories, CommentType } from './enums';
+import { CommentType } from './enums';
 
 export type CommentDocument = Comment & Document;
-
-// The GithubApp don't have access to the repository and user informations like the repository_id on ObjectId
 
 @Schema()
 export class Comment {
   @ApiProperty()
-  @Prop({ type: Types.ObjectId, ref: Repository.name, required: true })
-  repository_id: Types.ObjectId;
-
-  @ApiProperty()
-  @Prop({ type: Types.ObjectId, ref: User.name, required: true })
-  user_id: Types.ObjectId;
-
-  @ApiProperty()
-  @Prop({ required: true })
+  @Prop({ required: true, unique: true, type: String })
   gh_comment_id: string;
 
   @ApiProperty()
-  @Prop({ required: true })
-  repo_full_name: string;
+  @Prop({ required: true, type: String })
+  content: string;
 
   @ApiProperty()
-  @Prop({ required: true })
-  original_text: string;
+  @Prop({ required: true, type: Date })
+  comment_created_at: Date;
 
   @ApiProperty()
-  @Prop({ required: true })
+  @Prop({ required: true, type: String })
+  author_id: string;
+
+  @ApiProperty()
+  @Prop({ type: Object, required: true })
+  parent: {
+    type: CommentType;
+    gh_parent_id: number;
+    title: string;
+    url: string;
+  };
+
+  @ApiProperty()
+  @Prop({ required: true, type: String })
+  repository_fullname: string;
+
+  @ApiProperty()
+  @Prop({ required: true, type: Boolean })
+  is_repository_private: boolean;
+
+  @ApiProperty()
+  @Prop({ required: true, type: String })
+  repository_owner: string;
+
+  @ApiProperty()
+  @Prop({ required: true, type: Number })
   toxicity_score: number;
 
   @ApiProperty()
-  @Prop({ required: true })
-  is_post_installation: boolean;
+  @Prop({ required: true, type: Date })
+  toxicity_analyzed_at: Date;
 
   @ApiProperty()
-  @Prop({ required: true, enum: Categories })
-  classification: Categories;
+  @Prop({ required: true, type: Boolean, default: false })
+  flagged: boolean;
 
   @ApiProperty()
-  @Prop({ type: Types.ObjectId, ref: 'Suggestion' })
-  selected_suggestion_id: Types.ObjectId;
+  @Prop({ required: true, type: String })
+  classification: string;
 
   @ApiProperty()
-  @Prop({ required: true, enum: CommentType })
-  comment_type: CommentType;
+  @Prop({ required: true, type: Boolean, default: false })
+  solutioned: boolean;
+
+  @ApiProperty()
+  @Prop({ required: false, type: String, default: '' })
+  solution: string;
+
+  @ApiProperty()
+  @Prop({ required: false, type: Date, default: null })
+  solution_analyzed_at: Date;
+
+  @ApiProperty()
+  @Prop({ required: false, type: Types.ObjectId, default: null })
+  solution_id: Types.ObjectId;
+
+  @ApiProperty()
+  @Prop({ required: true, type: String })
+  event_type: string;
+
+  @ApiProperty()
+  @Prop({ required: true, type: Number })
+  installation_id: number;
+
+  @ApiProperty()
+  @Prop({ required: true, type: Boolean })
+  moderated: boolean;
+
+  @ApiProperty()
+  @Prop({ required: false, type: String })
+  moderation_action: string;
 }
 
 export const CommentSchema = SchemaFactory.createForClass(Comment);
