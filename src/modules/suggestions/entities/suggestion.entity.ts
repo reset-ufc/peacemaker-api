12ba9a41c279452a30564101834e3c51da98f068
Emanuel Avila <avilaemanueel@gmail.com>
2025-03-09 20:40:06 -0300

    altera a estrutura da entidade Suggestion para suportar múltiplas sugestões e adiciona índice de sugestão selecionada

--- src/modules/suggestions/entities/suggestion.entity.ts
+++ src/modules/suggestions/entities/suggestion.entity.ts
@@ -1,31 +1,28 @@
-import { Comment } from '@/modules/comments/entities/comment.entity';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
-import { Document, Types } from 'mongoose';
+import { Document } from 'mongoose';
 
 export type SuggestionDocument = Suggestion & Document;
 
 @Schema()
 export class Suggestion {
   @ApiProperty()
-  @Prop({ type: Types.ObjectId, ref: Comment.name, required: true })
-  comment_id: Types.ObjectId;
-
-  @ApiProperty()
   @Prop({ required: true })
   gh_comment_id: string;
 
   @ApiProperty()
   @Prop({ required: true })
-  content: string;
+  suggestions: Array<{
+    content: string; // solução sugerida
+  }>;
 
   @ApiProperty()
   @Prop({ default: false })
   is_edited: boolean;
 
   @ApiProperty()
-  @Prop({ default: false })
-  is_selected: boolean;
+  @Prop({ default: null })
+  suggestion_selected_index: number;
 
   @ApiProperty()
   @Prop({ default: Date.now })
